970130a2ef48a3e2ab8dc1c2a3001681d29badd1
kalmansour <63953899+kalmansour@users.noreply.github.com>
2022-04-07 14:54:54 +0300

    calculator input variables, user input validators for first number, second number and mathematical operations

--- calculator.py
+++ calculator.py
@@ -1,6 +1,22 @@
 
 def main():
-	#write your code here
+	while True:
+		try:
+			first_number = int(input("Enter the first number:"))
+			second_number = int(input("Enter the second number:"))
+			operation = input("Choose the operation (+, -, /, *):")
+			calculator = eval(f'{first_number}{operation}{second_number}')
+
+			while operation not in ['+', '-', '/','*']:
+				break
+			else:
+				print(f'The answer is {calculator}')
+			break
+		except (ValueError): 
+			print("Oops!  That was not a valid number.  Try again...")
+		except (SyntaxError): 
+			print("Oops!  That was not a valid operation.  Try again...")
+		
 	pass
 	
 
